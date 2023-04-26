5937cea5143d56546e6df5aa7453d6ae621064af
Tim Stannard <stannard.tim@gmail.com>
2023-04-26 14:25:13 +1200

    first

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("hello");
+// console.log("Hello");
+
+document.getElementById('greeting-h1').innerHTML = "Hello, Tim";
+
+document.getElementById('subheader').innerHTML = "A new subheader!";
+
+// Generate a list using Javascript
+
+document.getElementById('list-container').innerHTML = `
+<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+</ul>
+`;
